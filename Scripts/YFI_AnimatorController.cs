38580aa184764a94b507d1af31eda1da5e1fa363
西改改Yuxi <the_west@outlook.com>
2022-05-30 19:42:11 +0800

    Initial commit

---
+++ Scripts/YFI_AnimatorController.cs
@@ -0,0 +1,102 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class YFI_AnimatorController : UdonSharpBehaviour
+{
+    [Tooltip("Flight Data Interface")]
+    public UdonSharpBehaviour YFI_FlightDataInterface;
+    [Tooltip("仪表的动画控制器")]
+    public Animator IndicatorAnimator;
+
+    [Tooltip("速度表最大量程(节)")]
+    public int MAXSPEED = 500;
+    [Tooltip("俯仰角最大量程(度)")]
+    public int MAXPITCH = 20;
+    [Tooltip("滚转角最大量程(度)")]
+    public int MAXBANK = 45;
+    [Tooltip("高度表最大量程(英尺)")]
+    public int MAXALT = 10000;
+    [Tooltip("爬升率最大量程(英尺/分钟)")]
+    public int MAXVS = 16000;
+    //暂时不考虑航向表有量程
+
+    //一些需要提前算好的参数
+    private float pitchFixValue = 3.5f;
+    private float bankFixValue = 0.5f;
+    private float MAXPITCH_double = 40f;
+    private float MAXVS_double = 32000f;
+    private float MAXBANK_double = 90f;
+    //animator strings that are sent every frame are converted to int for optimization
+    private int AIRSPEED_HASH = Animator.StringToHash("AirSpeedNormalize");
+    private int PITCH_HASH = Animator.StringToHash("PitchAngelNormalize");
+    private int BANK_HASH = Animator.StringToHash("BankAngelNormalize");
+    private int ALT_HASH = Animator.StringToHash("AltitudeNormalize");
+    private int ROC_HASH = Animator.StringToHash("VerticalSpeedNormalize");
+    private int HEADING_HASH = Animator.StringToHash("HeadingNormalize");
+    /*
+    private int YAWINPUT_STRING = Animator.StringToHash("yawinput");
+    private int ROLLINPUT_STRING = Animator.StringToHash("rollinput");
+    private int THROTTLE_STRING = Animator.StringToHash("throttle");
+    private int ENGINEOUTPUT_STRING = Animator.StringToHash("engineoutput");
+    private int VTOLANGLE_STRING = Animator.StringToHash("vtolangle");
+    private int HEALTH_STRING = Animator.StringToHash("health");
+    private int AOA_STRING = Animator.StringToHash("AoA");
+    private int MACH10_STRING = Animator.StringToHash("mach10");
+    private int GS_STRING = Animator.StringToHash("Gs");
+    private int FUEL_STRING = Animator.StringToHash("fuel");
+    */
+    private void Start()
+    {
+        MAXPITCH_double = 2f * MAXPITCH;
+        MAXBANK_double = 2f * MAXBANK;
+        MAXVS_double = 2f * MAXVS;
+
+        pitchFixValue = (90f - MAXPITCH)/ MAXPITCH_double;
+        bankFixValue = (90f - MAXBANK) / MAXBANK_double;
+    }
+    private void LateUpdate()
+    {
+        //AirSpeed
+        IndicatorAnimator.SetFloat(AIRSPEED_HASH, (float)YFI_FlightDataInterface.GetProgramVariable("TAS")/MAXSPEED);
+        
+        //Altitude
+        IndicatorAnimator.SetFloat(ALT_HASH, (float)YFI_FlightDataInterface.GetProgramVariable("Altitude") / MAXALT);
+
+        //VS
+        float VerticalSpeedNormal = (float)YFI_FlightDataInterface.GetProgramVariable("RateOfClimb") / MAXVS_double + 0.5f;
+        if (0.0f < VerticalSpeedNormal && VerticalSpeedNormal < 1.0f)
+        { 
+            IndicatorAnimator.SetFloat(ROC_HASH, VerticalSpeedNormal); 
+        }
+
+        //Heading
+        IndicatorAnimator.SetFloat(HEADING_HASH, (float)YFI_FlightDataInterface.GetProgramVariable("Heading") / 360f);
+        
+        //Pitch
+        float PitchAngle90 = ((float)YFI_FlightDataInterface.GetProgramVariable("Pitch")  < 270f) ?
+            ((float)YFI_FlightDataInterface.GetProgramVariable("Pitch") + 90f) :
+            ((float)YFI_FlightDataInterface.GetProgramVariable("Pitch") - 270f);
+
+        float PitchNormal = PitchAngle90 / MAXPITCH_double - pitchFixValue;
+        if (0.0f < PitchNormal && PitchNormal < 1.0f)
+        { 
+            IndicatorAnimator.SetFloat(PITCH_HASH, PitchNormal); 
+        }
+
+        //Bank
+        float BankAngle90 = ((float)YFI_FlightDataInterface.GetProgramVariable("Bank") < 270f) ?
+            ((float)YFI_FlightDataInterface.GetProgramVariable("Bank") + 90f) :
+            ((float)YFI_FlightDataInterface.GetProgramVariable("Bank") - 270f);
+
+        float BankNormal = BankAngle90 / MAXBANK_double - bankFixValue;
+        if (0.0f < BankNormal && BankNormal < 1.0f)
+        {    
+            IndicatorAnimator.SetFloat(BANK_HASH, BankNormal); 
+        }
+
+    }
+
+}
---
+++ Scripts/YFI_FlightDataInterface.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+using UdonSharp;
+using VRC.SDKBase;
+using VRC.Udon;
+using UnityEngine.UI;
+
+[UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+public class YFI_FlightDataInterface : UdonSharpBehaviour
+{
+    /*2022-05-28
+     * 我寻思先写一个接口从AVController或者eneity里面把仪表
+     * 需要显示的参数先给整出来
+     * 然后之后的仪表从这里读数据，生成transform
+     */
+    [Tooltip("这里是SAVControl")]
+    public UdonSharpBehaviour SAVControl;
+    //[Tooltip("用动画器做仪表变形的控制")]
+    [Tooltip("Debug Output Text")]
+    public Text DebugOut;
+    
+    private SaccEntity EntityControl;
+    private Transform VehicleTransform;
+    VRCPlayerApi localPlayer;
+
+    private Transform CenterOfMass;
+    //下面定义了关键的飞行参数们
+    private Vector3 VelocityVector;
+    private float GValue;
+    private float AngleOfAttack;
+    private float GS;
+    private float Mach;
+    private float TAS;
+    private float IAS;
+    private float RateOfClimb;
+    private float Altitude;
+    private float Heading;
+    private float Pitch;
+    private float Bank;
+    private float SeaLevel; 
+
+
+    private float DeltaTime;
+    private Vector3 DeltaVelocityVector;
+    private Vector3 VelocityVectorBefore;
+    private Vector3 PerdCurrentVelocityVector;
+    private Vector3 LerpCurrentVelocityVector;
+    private float TimeBefore;
+
+    //方法
+    private void Start()
+    {
+
+        EntityControl = (SaccEntity)SAVControl.GetProgramVariable("EntityControl");
+        SeaLevel = (float)SAVControl.GetProgramVariable("SeaLevel");
+        localPlayer = Networking.LocalPlayer;
+        CenterOfMass = EntityControl.CenterOfMass;
+    }
+
+    private void OnEnable()
+    {
+        return;
+    }
+    private void Update()
+    {
+        //TODO:如果性能出现问题，限制此处的更新频率
+        //我觉得时间平滑不用在这里实现，但是肯定要考虑不是每一个tick都调用以减少开销
+        //float SmoothDeltaTime = Time.smoothDeltaTime;
+
+        //获取速度向量
+        //SAV中CurrentVel是飞行器速度向量，本质上小飞机所在位置就是1s(?)后的飞机位置
+        Vector3 CurrentVelocityVector = (Vector3)SAVControl.GetProgramVariable("CurrentVel");
+
+        //(在绘制AHI时再做投影)
+        //低速时，让箭头垂直向下
+        if (CurrentVelocityVector.magnitude < 2)
+        {
+            CurrentVelocityVector = Vector3.down * 2;//straight down instead of spazzing out when moving very slow
+        }
+        else 
+        {
+            //插值操作，做平滑
+            if (CurrentVelocityVector != VelocityVectorBefore)
+            {
+                float CurrentTime = Time.time;
+                DeltaTime = CurrentTime - TimeBefore;
+                DeltaVelocityVector = (CurrentVelocityVector - VelocityVectorBefore) / DeltaTime;
+
+                VelocityVectorBefore = CurrentVelocityVector;
+                TimeBefore = CurrentTime;
+            }
+            //VelocityVector = CurrentVelocityVector + DeltaVelocityVector * (Time.time- TimeBefore);
+            PerdCurrentVelocityVector = CurrentVelocityVector + DeltaVelocityVector * (DeltaTime);
+        }
+
+        //TODO:这玩意是否应该在仪表脚本里(如果有)完成
+        //TODO:是否需要正则化？
+        //可能需要(如果要投影在一个机械的陀螺仪（球)上)
+        if ((bool)SAVControl.GetProgramVariable("IsOwner"))
+        {
+            VelocityVector = PerdCurrentVelocityVector;
+        }
+        else
+        {
+            LerpCurrentVelocityVector = Vector3.Lerp(LerpCurrentVelocityVector, PerdCurrentVelocityVector, 9f * Time.smoothDeltaTime);
+            VelocityVector = LerpCurrentVelocityVector;
+        }
+
+        //////////
+
+        Vector3 VehicleEuler = EntityControl.transform.rotation.eulerAngles;
+        //航向
+        //Heading = (Quaternion.Euler(new Vector3(0, -VehicleEuler.y, 0)));
+        //float angle = Quaternion.Angle(transform.rotation, target.rotation);
+        Heading = VehicleEuler.y;
+        //俯仰
+        //Pitch = Quaternion.Euler(new Vector3(0, VehicleEuler.y, 0));
+        Pitch = VehicleEuler.x;
+        //坡度
+        //Bank = Quaternion.Euler(new Vector3(0, 0, -VehicleEuler.z));
+        Bank = VehicleEuler.z;
+        //过载
+        GValue = ((float)SAVControl.GetProgramVariable("VertGs"));
+        //攻角
+        AngleOfAttack = ((float)SAVControl.GetProgramVariable("AngleOfAttack"));
+        //马赫数
+        Mach = ((float)SAVControl.GetProgramVariable("Speed")) / 343f;
+        //爬升率
+        RateOfClimb = ((Vector3)SAVControl.GetProgramVariable("CurrentVel")).y * 60 * 3.28084f;
+        //高度
+        Altitude = (CenterOfMass.position.y - SeaLevel) * 3.28084f;
+
+        //SAV里的speed是地速 airspeed是TAS
+        //SAV里使用的是IS，所以这里要把秒换算为节
+        //地速
+        GS = ((float)SAVControl.GetProgramVariable("Speed")) *1.9438445f;
+        //真空速
+        TAS = ((float)SAVControl.GetProgramVariable("AirSpeed")) *1.9438445f;
+
+        if (DebugOut)
+        {
+            //HUDText_G.text = string.Concat(((float)SAVControl.GetProgramVariable("VertGs")).ToString("F1"), "\n", maxGs.ToString("F1"));
+            DebugOut.text = string.Concat("Headin: ", Heading.ToString(), 
+            "\nPitch: ", Pitch.ToString(),
+            "\nBank: ", Bank.ToString(),
+            "\nG: ", GValue.ToString(),
+            "\nAOA: ", AngleOfAttack.ToString(),
+            "\nMach: ", Mach.ToString(),
+            "\nROC: ", RateOfClimb.ToString(),
+            "\nAltitude: ", Altitude.ToString(),
+            "\nGS: ", GS.ToString(),
+            "\nTAS: ", TAS.ToString());
+        }
+    }
+}
