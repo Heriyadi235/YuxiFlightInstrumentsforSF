dda0360fbfbc1b10f9cc8692780d268f01d80602
西改改Yuxi <the_west@outlook.com>
2022-10-23 22:10:06 +0800

    autopilot

---
+++ AutoPilot/script/YFI_AutoPilot.cs
@@ -0,0 +1,140 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+using TMPro;
+
+namespace YuxiFlightInstruments
+{
+    //TODO:我也不知道这么更新对不对
+    //原则上可以不用更新，但是希望实现一个其他人也能看到ap上文字的效果
+    [DefaultExecutionOrder(1000)]
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class YFI_AutoPilot : UdonSharpBehaviour
+    {
+
+        public YFI_SPDControl SPDControl;
+        public UdonSharpBehaviour HDGControl;
+        public UdonSharpBehaviour ALTControl;
+
+        public TextMeshPro SPDText;
+        public TextMeshPro HDGText;
+        public TextMeshPro ALTText;
+        public TextMeshPro VSText;
+
+        public GameObject SPDEnableIndicator;
+        public GameObject HDGEnableIndicator;
+        public GameObject ALTEnableIndicator;
+
+        public float SPDCommand = 200f;
+        public float HDGCommand = 10f;
+        public float ALTCommand = 2000f;
+        public float VSCommand = 1500f;
+
+        public float minALT = 500f;
+        public float maxALT = 50000f;
+        public float ALTStep = 500f;
+
+        public float minSPD = 30f;
+        public float maxSPD = 400f;
+        public float SPDStep = 25f;
+
+        private bool isAPEnable = false; //处于某些原因先放一个总开关在这
+        private bool isSPDEnable = false;
+        private bool isHDGEnable = false;
+        private bool isALTEnable = false;
+        //之后会加入的显示格式化功能
+        //public bool overrideFrequencyFormat = false;
+        //public string frequencyFormat = "{0:#00.00#}";
+        void Start()
+        {
+            UpdateIndicator();
+            UpdateDisplay();
+            if (SPDControl)
+            {
+                SPDControl.SetSpeed = SPDCommand;
+                SPDControl.gameObject.SetActive(isALTEnable);
+            }
+        }
+
+        private void UpdateIndicator()
+        {
+            if (SPDEnableIndicator != null)
+                SPDEnableIndicator.SetActive(isSPDEnable);
+            if (HDGEnableIndicator != null)
+                HDGEnableIndicator.SetActive(isHDGEnable);
+            if (ALTEnableIndicator != null)
+                ALTEnableIndicator.SetActive(isALTEnable);
+        }
+
+        private void UpdateDisplay()
+        {
+            //if (frequencyText && !overrideFrequencyFormat) frequencyFormat = frequencyText.text;
+            if (SPDText)
+                SPDText.text = string.Format("{0:###}", SPDCommand);
+            if (ALTText)
+                ALTText.text = string.Format("{0:#####}", ALTCommand);
+            if (VSText)
+                VSText.text = string.Format("{0:####}", VSCommand);
+            if (HDGText)
+                HDGText.text = string.Format("{0:###}", HDGCommand);
+
+        }
+
+        public void _TakeOwnership()
+        {
+            if (Networking.IsOwner(gameObject)) return;
+            Networking.SetOwner(Networking.LocalPlayer, gameObject);
+        }
+
+        public void _ToggleALT()
+        {
+            _TakeOwnership();
+            if(ALTControl)
+                ALTControl.gameObject.SetActive(isALTEnable);
+            isALTEnable = !isALTEnable;
+            UpdateIndicator();
+            RequestSerialization();
+        }
+        public void _SetALT(float altitude)
+        {
+            _TakeOwnership();
+            altitude = altitude > maxALT ? minALT : (altitude < minALT ? maxALT : altitude);
+            RequestSerialization();
+        }
+        public void _ALTIncrease() => _SetALT(ALTCommand + ALTStep);
+        public void _ALTDecrease() => _SetALT(ALTCommand - ALTStep);
+
+        public void _ToggleSPD()
+        {
+            _TakeOwnership();
+            isSPDEnable = !isSPDEnable;
+            if (SPDControl)
+            {
+                SPDControl.gameObject.SetActive(isALTEnable);
+                SPDControl.SetSpeed = SPDCommand / 1.9438445f;
+                if (isSPDEnable)
+                    SPDControl.SetCruiseOn();
+                else
+                    SPDControl.SetCruiseOff();
+            }
+            UpdateIndicator();
+            RequestSerialization();
+        }
+        public void _SetSPD(float speed)
+        {
+            _TakeOwnership();
+            SPDCommand = speed > maxSPD ? minSPD : (speed < minSPD ? maxSPD : speed);
+            UpdateDisplay();
+            //SPDControl.SetProgramVariable("SetSpeed", SPDCommand);
+            SPDControl.SetSpeed = SPDCommand / 1.9438445f;
+            RequestSerialization();
+        }
+        public void _SPDIncrease() => _SetSPD(SPDCommand + SPDStep);
+        public void _SPDDecrease() => _SetSPD(SPDCommand - SPDStep);
+
+        //一堆事件处理函数
+
+    }
+}
---
+++ AutoPilot/script/YFI_SPDControl.cs
@@ -0,0 +1,255 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using UnityEngine.UI;
+using VRC.SDKBase;
+using VRC.Udon;
+using SaccFlightAndVehicles;
+
+namespace YuxiFlightInstruments
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class YFI_SPDControl : UdonSharpBehaviour
+    {
+        [SerializeField] UdonSharpBehaviour SAVControl;
+        [Tooltip("Object enabled when function is active (used on MFD)")]
+        public GameObject Dial_Funcon;
+        public bool AllowCruiseGrounded;
+        //public Text HUDText_knotstarget;
+        private SaccEntity EntityControl;
+        private bool UseLeftTrigger = false;
+        private bool TriggerLastFrame;
+        private Transform VehicleTransform;
+        private VRCPlayerApi localPlayer;
+        private float CruiseTemp;
+        private float SpeedZeroPoint;
+        private float TriggerTapTime = 1;
+        [System.NonSerializedAttribute] public bool Cruise;
+        public float CruiseProportional = .1f;
+        public float CruiseIntegral = .1f;
+        private float CruiseIntegrator;
+        private float CruiseIntegratorMax = 5;
+        private float CruiseIntegratorMin = -5;
+        private float Cruiselastframeerror;
+        //
+        public float error = 0f;
+        private bool func_active;
+        private bool Selected;
+        private bool Piloting;
+        //
+        private bool EngineOn;
+        private bool InVTOL;
+        private bool InReverse;
+        private bool InVR;
+        private bool CruiseThrottleOverridden;
+        private Transform ControlsRoot;
+        [System.NonSerializedAttribute] public float SetSpeed = 0f;
+        public void DFUNC_LeftDial() { UseLeftTrigger = true; }
+        public void DFUNC_RightDial() { UseLeftTrigger = false; }
+        public void SFEXT_L_EntityStart()
+        {
+            localPlayer = Networking.LocalPlayer;
+            if (localPlayer != null)
+            { InVR = localPlayer.IsUserInVR(); }
+            EntityControl = (SaccEntity)SAVControl.GetProgramVariable("EntityControl");
+            ControlsRoot = (Transform)SAVControl.GetProgramVariable("ControlsRoot");
+            VehicleTransform = EntityControl.transform;
+            if (Dial_Funcon) Dial_Funcon.SetActive(false);
+        }
+        /*
+        public void DFUNC_Selected()
+        {
+            TriggerLastFrame = true;
+            gameObject.SetActive(true);
+            Selected = true;
+        }
+        public void DFUNC_Deselected()
+        {
+            if (!Cruise) { gameObject.SetActive(false); }
+            TriggerTapTime = 1;
+            Selected = false;
+        }
+        */
+        public void SFEXT_O_PilotEnter()
+        {
+            if (Dial_Funcon) Dial_Funcon.SetActive(Cruise);
+            Piloting = true;
+            //if (HUDText_knotstarget) { HUDText_knotstarget.text = string.Empty; }
+        }
+        public void SFEXT_P_PassengerEnter()
+        {
+            if (Dial_Funcon) Dial_Funcon.SetActive(Cruise);
+            //if (HUDText_knotstarget) { HUDText_knotstarget.text = string.Empty; }
+        }
+        public void SFEXT_O_PilotExit()
+        {
+            Piloting = false;
+            TriggerTapTime = 1;
+            Selected = false;
+        }
+        public void SFEXT_G_EngineOn()
+        {
+            EngineOn = true;
+        }
+        public void SFEXT_G_EngineOff()
+        {
+            EngineOn = false;
+            gameObject.SetActive(false);
+            func_active = false;
+            if (Cruise)
+            {
+                SetCruiseOff();
+            }
+        }
+        public void SFEXT_G_Explode()
+        {
+            if (Cruise)
+            { SetCruiseOff(); }
+        }
+        public void SFEXT_G_TouchDown()
+        {
+            if (Cruise && !AllowCruiseGrounded)
+            { SetCruiseOff(); }
+        }
+        public void SFEXT_O_EnterVTOL()
+        {
+            if (Cruise)
+            { SetCruiseOff(); }
+            InVTOL = true;
+        }
+        public void SFEXT_O_ExitVTOL()
+        { InVTOL = false; }
+        public void SFEXT_O_StartReversing()
+        {
+            if (Cruise)
+            { SetCruiseOff(); }
+            InReverse = true;
+        }
+        public void SFEXT_O_StopReversing()
+        { InReverse = false; }
+        private void LateUpdate()
+        {
+            //if (EngineOn)
+            if (true)
+            { 
+                {
+                    bool ShiftCtrl = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.LeftControl);
+                    if (ShiftCtrl)
+                    {
+                        if (CruiseThrottleOverridden)
+                        {
+                            SAVControl.SetProgramVariable("ThrottleOverridden", (int)SAVControl.GetProgramVariable("ThrottleOverridden") - 1);
+                            CruiseThrottleOverridden = false;
+                        }
+                    }
+                    else
+                    {
+                        if (Cruise)
+                        {
+                            if (!CruiseThrottleOverridden)
+                            {
+                                SAVControl.SetProgramVariable("ThrottleOverridden", (int)SAVControl.GetProgramVariable("ThrottleOverridden") + 1);
+                                CruiseThrottleOverridden = true;
+                            }
+                        }
+                    }
+                }
+                float DeltaTime = Time.deltaTime;
+                //float equals = Input.GetKey(KeyCode.Equals) ? DeltaTime * 10 : 0;
+                //float minus = Input.GetKey(KeyCode.Minus) ? DeltaTime * 10 : 0;
+                //SetSpeed = Mathf.Max(SetSpeed + (equals - minus), 0);
+
+                if (func_active)
+                {
+                    //float error = (SetSpeed - (float)SAVControl.GetProgramVariable("AirSpeed"));
+                    error = (SetSpeed - (float)SAVControl.GetProgramVariable("AirSpeed"));
+                    CruiseIntegrator += error * DeltaTime;
+                    CruiseIntegrator = Mathf.Clamp(CruiseIntegrator, CruiseIntegratorMin, CruiseIntegratorMax);
+
+                    //float Derivator = Mathf.Clamp(((error - lastframeerror) / DeltaTime),DerivMin, DerivMax);
+
+                    SAVControl.SetProgramVariable("ThrottleOverride", Mathf.Clamp((CruiseProportional * error) + (CruiseIntegral * CruiseIntegrator), 0, 1));
+                    //ThrottleInput += Derivative * Derivator; //works but spazzes out real bad
+
+                    TriggerTapTime += DeltaTime;
+                }
+            }
+
+        }
+        public void KeyboardInput()
+        {
+            if (!Cruise)
+            {
+                if ((!(bool)SAVControl.GetProgramVariable("Taxiing") || AllowCruiseGrounded) && !InVTOL && !InReverse)
+                { SetCruiseOn(); }
+            }
+            else
+            {
+                SetCruiseOff();
+            }
+        }
+        public void SetCruiseOn()
+        {
+            if (Cruise) { return; }
+            if (Piloting)
+            {
+                func_active = true;
+                if (!InVR)
+                { gameObject.SetActive(true); }
+            }
+            if (!CruiseThrottleOverridden)
+            {
+                SAVControl.SetProgramVariable("ThrottleOverridden", (int)SAVControl.GetProgramVariable("ThrottleOverridden") + 1);
+                CruiseThrottleOverridden = true;
+            }
+            //SetSpeed = (float)SAVControl.GetProgramVariable("AirSpeed");
+            Cruise = true;
+            if (Dial_Funcon) { Dial_Funcon.SetActive(true); }
+            EntityControl.SendEventToExtensions("SFEXT_O_CruiseEnabled");
+        }
+        public void SetCruiseOff()
+        {
+            if (!Cruise) { return; }
+            if (Piloting)
+            {
+                func_active = false;
+                if (!InVR)
+                { gameObject.SetActive(false); }
+            }
+            if (CruiseThrottleOverridden)
+            {
+                SAVControl.SetProgramVariable("ThrottleOverridden", (int)SAVControl.GetProgramVariable("ThrottleOverridden") - 1);
+                CruiseThrottleOverridden = false;
+            }
+            TriggerTapTime = 1;
+            SAVControl.SetProgramVariable("PlayerThrottle", (float)SAVControl.GetProgramVariable("ThrottleInput"));
+            Cruise = false;
+            if (Dial_Funcon) { Dial_Funcon.SetActive(false); }
+            //if (HUDText_knotstarget) { HUDText_knotstarget.text = string.Empty; }
+            EntityControl.SendEventToExtensions("SFEXT_O_CruiseDisabled");
+        }
+        public void SFEXT_O_ThrottleDropped()
+        {
+            if (!CruiseThrottleOverridden && Cruise)
+            {
+                SAVControl.SetProgramVariable("ThrottleOverridden", (int)SAVControl.GetProgramVariable("ThrottleOverridden") + 1);
+                CruiseThrottleOverridden = true;
+            }
+        }
+        public void SFEXT_O_ThrottleGrabbed()
+        {
+            if (CruiseThrottleOverridden)
+            {
+                SAVControl.SetProgramVariable("ThrottleOverridden", (int)SAVControl.GetProgramVariable("ThrottleOverridden") - 1);
+                CruiseThrottleOverridden = false;
+            }
+        }
+        public void SFEXT_O_LoseOwnership()
+        {
+            gameObject.SetActive(false);
+            func_active = false;
+            if (Cruise)
+            { SetCruiseOff(); }
+        }
+    }
+}
